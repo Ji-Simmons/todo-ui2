99fb6969f5ef1a3382cc6052205452d07523ff5f
Conni Simmons <connisimmons@gmail.com>
2020-07-23 20:10:23 -0600

    all but task desk

--- src/App.js
+++ src/App.js
@@ -1,26 +1,146 @@
 import React from 'react';
-import logo from './logo.svg';
+import ListForm from './components/ListForm';
 import './App.css';
+import Context from './context';
+import { uuid } from './helpers';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			tasks: [],
+			taskList: [],
+			editForm: '',
+			listId: '',
+			lists: [],
+			listElements: [],
+			listTasks: [],
+			thingToDisplay: null,
+		};
+	}
+
+	getTasks = async () => {
+		return await fetch('http://localhost:5555/api/tasks')
+			.then((response) => {
+				return response.json();
+			})
+			.then((data) => {
+				console.log('data from api: ', data);
+				this.setState({
+					tasks: data,
+					taskList: data.map((item) => {
+						return (
+							<li key={item._id} id={item._id} onClick={this.updateTask}>
+								{item.username || 'Unknown'}
+							</li>
+						);
+					}),
+					editForm: ''
+				});
+			})
+			.catch();
+	};
+
+	getLists = () => {
+		fetch('http://localhost:5555/api/lists')
+			.then((response) => {
+				return response.json();
+			})
+			.then((data) => {
+				console.log('lists from api: ', data);
+				this.setState({
+					lists: data,
+					listElements: data.map((item) => {
+						return (
+							<div key={item._id}>
+								<li id={item._id} onClick={this.updateListDisplay}>
+									{item.name || 'Unknown'}
+								</li>
+								<button className="deleteButton" id={item._id} onClick={this.deleteList}>
+									X
+								</button>
+							</div>
+						);
+					}),
+					editForm: null,
+					thingToDisplay: null
+				});
+			})
+			.catch();
+	};
+
+	updateListDisplay = (event) => {
+		event.preventDefault();
+		let id = event.target.getAttribute('id');
+		let list = this.state.lists.filter((list) => {
+			return list._id === id;
+		});
+		// because this returns an array and i only want the first one...
+		list = list[0];
+		this.setState(
+			{
+				thingToDisplay: null
+			},
+			function() {
+				this.setState({
+					thingToDisplay: <ListForm key={uuid()} {...list} />
+				});
+			}
+		);
+	};
+	deleteList = (event) => {
+		console.log('deleting...');
+		event.preventDefault();
+		let id = event.target.getAttribute('id');
+		console.log('id: ', id);
+		let fetchOptions = {
+			method: 'DELETE'
+		};
+		fetch(`http://localhost:5555/api/lists/${id}`, fetchOptions)
+			.then((response) => {
+				return response.json();
+			})
+			.then((data) => {
+				console.log('response from api: ', data);
+				this.getLists();
+			})
+			.catch();
+	};
+
+	componentDidMount() {
+		this.getTasks();
+		this.getLists();
+	}
+
+	doCreateListThing = (event) => {
+		event.preventDefault();
+		this.setState({
+			thingToDisplay: <ListForm key={uuid()} />
+		});
+  };
+  
+
+
+	// This uses a single global context to share the state of the App compontent to everything else
+	// I should be ashamed of myself for doing this
+	render() {
+		return (
+			<div className="App">
+				<Context.Provider value={{ ...this.state, getLists: this.getLists, getTasks: this.getTasks }}>
+					<section className="controls">
+						<section className="buttons">
+							<button onClick={this.doCreateListThing}>Create List</button>
+						</section>
+						<section className="my-lists">
+							<h2>My Lists</h2>
+							<ul>{this.state.listElements}</ul>
+						</section>
+					</section>
+					<section className="content"> {this.state.thingToDisplay}</section>
+				</Context.Provider>
+			</div>
+		);
+	}
 }
 
 export default App;
---
+++ src/components/TaskForm.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import Context from '../context';
+import { uuid } from '../helpers';
+import TaskList from './TaskList';
+
+const TASK_STATUSES = [ 'pending', 'complete' ];
+
+export default class TaskForm extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+            listId: props.listId || 'error',
+			name: props.name || '',
+			description: props.description || '',
+			due: props.due || '',
+			status: props.status || 'pending'
+		};
+	}
+	changeHandler = (event) => {
+		let field = event.target.getAttribute('name');
+		let stateObject = {};
+		stateObject[field] = event.target.value;
+		this.setState(stateObject);
+	};
+
+	handleSubmit = (event) => {
+		event.preventDefault();
+        // and some other stuff
+        let route = `http://localhost:5555/api/tasks`;
+        let options = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(this.state),
+        };
+        fetch(route, options)
+        .then((res) => { return res.json()})
+        .then((data) =>
+        {
+            console.log('should have added a new task: ', data);
+            this.context.getTasks();
+        })
+        .catch((err) =>
+        {
+            console.log('might not have added a new task: ', err);
+        })
+	};
+	render() {
+		return (
+			<form onSubmit={this.handleSubmit}>
+				<div className="taskRow">
+
+					<input name="name" value={this.state.name} onChange={this.changeHandler} placeholder='Name' />
+
+					<input name="description" value={this.state.description} onChange={this.changeHandler} placeholder='Description' />
+
+					<input name="due" value={this.state.due} onChange={this.changeHandler} placeholder='due' />
+                    <input className="small" type="submit" value="Add Task" />
+                </div>
+			</form>
+		);
+	}
+}
+TaskForm.contextType = Context;
