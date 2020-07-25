eb2b3a1542dea46846aed3daa454cdfaa8ea17a3
Ji Simmons <ji.simmons@yahoo.com>
2020-07-24 19:20:51 -0600

    Updates

---
+++ precache-manifest.ac2a425530bb304afe21015344ff0f4c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70c9cef5a8c6e26ad002004206474407",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3154a632ee14ba4bce63",
+    "url": "/static/css/main.94683603.chunk.css"
+  },
+  {
+    "revision": "86e2160c27991ce214b3",
+    "url": "/static/js/2.4526c214.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4526c214.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3154a632ee14ba4bce63",
+    "url": "/static/js/main.d0a57fb9.chunk.js"
+  },
+  {
+    "revision": "8fe477f92f69e23c643b",
+    "url": "/static/js/runtime-main.8e5fb534.js"
+  }
+]);
